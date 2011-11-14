2b27156a3aef4c8c43f6f3490bb0b7b7ef4eb074
Rajamohamed J <rajamohamed.j@shriramits.com>
2011-11-14 16:19:10 +0530

    README

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < InheritedResources::Base
   before_filter :authenticate_user! 
-  respond_to :html, :xml
+  respond_to :html, :xml, :json
   has_scope :limit, :default => 8
   
   def create
